f76779953f02fea7ec0af90a2358464a3dcd1b50
Johanna Avila <joeannaavila123@gmail.com>
2024-01-04 19:02:59 -0600

    pycode adjustments

--- reinforcement_learning/q_learning/1-q_init.py
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -11,8 +11,8 @@ def q_init(env):
     # Get the number of states and actions from the environment
     num_states = env.observation_space.n
     num_actions = env.action_space.n
-    
+
     # Initialize the Q-table with zeros
     q_table = np.zeros((num_states, num_actions))
-    
+
     return q_table
