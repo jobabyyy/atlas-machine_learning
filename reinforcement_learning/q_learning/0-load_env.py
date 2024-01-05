7abbb29bf3408d63dba71f25bad5a33d5692ed25
Johanna Avila <joeannaavila123@gmail.com>
2024-01-04 18:29:35 -0600

    function accepts all given parameters

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -23,13 +23,7 @@ is_slippery is a boolean to determine if the ice is slippery
 Returns: the environment
     """
     # init conditonals to check parameters
-    if map_name is not None:
-        env = gym.make('FrozenLake-v1', map_name=map_name,
-                       is_slippery=is_slippery)
-    elif desc is not None:
-        env = gym.make('FrozenLake-v1', desc=desc,
-                        is_slippery=is_slippery)
-    else:
-        env = gym.make('FrozenLake-v1', is_slippery=is_slippery)
+    env = gym.make('FrozenLake-v1', desc=desc, map_name=map_name,
+                    is_slippery=is_slippery)
 
     return env
