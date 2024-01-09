becb851af51dbeaf4a17d86bd9153abe3d1e4703
Johanna Avila <joeannaavila123@gmail.com>
2024-01-09 14:50:07 -0600

    changed state to reset rather than set. added display for current state. moved loop down to for func.

--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -4,6 +4,7 @@ Agent to perform an episode."""
 
 
 import numpy as np
+import gym
 
 def play(env, Q, max_steps=100):
     """Function Play that has the trained
@@ -21,19 +22,23 @@ def play(env, Q, max_steps=100):
     """
     # awards player starts with
     total_rewards = 0
+    # current state
+    state = env.reset()
+
+
     for step in range(max_steps):
-      # current state
-      env = env.set()
+      # display current state
+      env.render()
 
       # action chosen using qtable
       action = np.agrmax(Q[state, :])
 
       # next action taken
-      next_state, reward, done = env.step(action)
+      next_state, reward, done, _ = env.step(action)
 
       # update total rewards
       total_rewards += reward
-      
+
       # next state
       state = next_state
 
@@ -46,19 +51,19 @@ def play(env, Q, max_steps=100):
 
     return total_rewards
 
-if __name__ == '__main__':
+    if __name__ == '__main__':
 
-    load_frozen_lake = __import__('0-load_env').load_frozen_lake
-    q_init = __import__('1-q_init').q_init
-    train = __import__('3-q_learning').train
-    play = __import__('4-play').play
+      load_frozen_lake = __import__('0-load_env').load_frozen_lake
+      q_init = __import__('1-q_init').q_init
+      train = __import__('3-q_learning').train
+      play = __import__('4-play').play
 
-    import numpy as np
+      import numpy as np
 
-    np.random.seed(0)
-    desc = [['S', 'F', 'F'], ['F', 'H', 'H'], ['F', 'F', 'G']]
-    env = load_frozen_lake(desc=desc)
-    Q = q_init(env)
+      np.random.seed(0)
+      desc = [['S', 'F', 'F'], ['F', 'H', 'H'], ['F', 'F', 'G']]
+      env = load_frozen_lake(desc=desc)
+      Q = q_init(env)
 
-    Q, total_rewards  = train(env, Q)
-    print(play(env, Q))
+      Q, total_rewards  = train(env, Q)
+      print(play(env, Q))
