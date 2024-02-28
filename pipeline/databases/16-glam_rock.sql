3c321c1d5f28786f0104205951ae741039f5bb3f
Johanna Avila <joeannaavila123@gmail.com>
2024-02-28 13:24:09 -0600

    lil baby syntax errors

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,5 +1,5 @@
 -- list all bands w/ Glam rock as their main style
 SELECT band_name, IFNULL((spilt - formed), (2020 - formed)) AS lifespan
 FROM metal_bands
-WHERE style LIKE %Glam rock%
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
