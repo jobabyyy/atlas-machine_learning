a15f2f83019baf985c5dd0ee56780f6150a8e9ec
Johanna Avila <joeannaavila123@gmail.com>
2024-02-28 16:14:39 -0600

    logging stats

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -14,14 +14,17 @@ def nginx_logs_stats():
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_collection = client.logs.nginx
 
+    # Total logs
     total_logs = logs_collection.count_documents({})
     print(f"{total_logs} logs")
-    print("Methods:")
 
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = logs_collection.count_documents({"method": method})
-        print(f"\t{method}: {count}")
+        print(f"\tmethod {method}: {count}")
 
-    status_count = logs_collection.count_documents({"method": "GET", "path": "/status"})
+    # Check status
+    status_count = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
     print(f"{status_count} documents with method=GET and path=/status")
