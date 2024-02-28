739de2c47d7810322cd7ab36bb76a7e8b79f9ab0
Johanna Avila <joeannaavila123@gmail.com>
2024-02-28 14:28:15 -0600

    safediv func created

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,13 @@
+-- function SafeDiv created to divide the 1 and 2 number or return 0
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10,2)
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END$$
+
+DELIMITER ;
