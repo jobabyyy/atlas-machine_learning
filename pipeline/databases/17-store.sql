21a923077627a665f624c4796445d454e95d2836
Johanna Avila <joeannaavila123@gmail.com>
2024-02-28 13:28:05 -0600

    literally spelled quantity wrong

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,7 +1,7 @@
--- trigger that decreases quanitity of an item after new order added
+-- trigger that decreases quantity of an item after new order added
 DROP trigger IF EXSITS after_order_insert;
 CREATE trigger after_order_insert AFTER INSERT ON orders
 FOR EACH ROW
     UPDATE items
-    SET quanitity = quanitity - NEW.number
+    SET quantity = quantity - NEW.number
     WHERE items.name = NEW.item_name;
