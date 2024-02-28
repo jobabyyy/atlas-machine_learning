ce6eb922678ce40b8a293d731188bd6044ca8afe
Johanna Avila <joeannaavila123@gmail.com>
2024-02-27 21:45:45 -0600

    switched genres in line 2 to ratings

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,5 +1,5 @@
 -- list all shows from db by their rating
-SELECT tv_shows.title AS title, SUM(tv_show_genres.rate) AS rating
+SELECT tv_shows.title AS title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id=tv_show_ratings.show_id
 GROUP BY tv_shows.title
