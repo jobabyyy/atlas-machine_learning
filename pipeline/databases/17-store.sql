1d9217d6e73e51b6349f766897677e506457ef12
Johanna Avila <joeannaavila123@gmail.com>
2024-02-28 13:41:02 -0600

    incorrect syntax

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,9 +1,10 @@
 -- trigger that decreases quantity of an item after new order added
 DROP TRIGGER IF EXSITS after_order_insert;
+
 CREATE TRIGGER after_order_insert
 AFTER INSERT ON orders
-BEGIN
 FOR EACH ROW
+BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE items.name = NEW.item_name;
