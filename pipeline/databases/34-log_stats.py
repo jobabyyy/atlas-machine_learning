3b38e73d4d6f5923dde7c7b15c04808f98e8ba03
Johanna Avila <joeannaavila123@gmail.com>
2024-02-28 16:22:05 -0600

    diff method

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -12,17 +12,18 @@ def nginx_logs_stats():
     provide stats about Ngnix logs stored in MongoDB:
     """
     client = MongoClient('mongodb://localhost:27017/')
-    logs_collection = client.logs.nginx
+    db = client['logs']
+    log_collection = db['nginx']
 
-    total_logs = logs_collection.count_documents({})
-    print(f"{total_logs} logs")
+    total_logs = log_collection.count_documents({})
+
+    print(f"first line: {total_logs} logs where {total_logs} is the number of documents in this log_collection")
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = logs_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        count = log_collection.count_documents({"method": method})
+        print(f"\t{count} logs with method={method}")
 
-    status_count = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{status_count} status check")
+    count_stat = log_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{count_stat} logs with method=GET\npath=/status")
