f8b9b7ddf9951851a89c017c538ac6f27c5a24a5
Johanna Avila <joeannaavila123@gmail.com>
2024-02-27 20:11:13 -0600

    lisitng shows by tv genre and title

---
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- list all shows in db by genre and title
+SELECT tv_shows.title, tv_shows_genres.genre_id
+FROM tv_shows
+JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
+ORDER BY tv_shows.title, tv_shows_genres.genre_id;
