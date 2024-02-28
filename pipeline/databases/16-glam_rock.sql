4d1fff73596864159f6f64b99a1a9179d482481e
Johanna Avila <joeannaavila123@gmail.com>
2024-02-28 13:54:00 -0600

    recalculating the lifespan

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,5 +1,5 @@
 -- list all bands w/ Glam rock as their main style
-SELECT band_name, IFNULL((spilt - formed), (2020 - formed)) AS lifespan
+SELECT band_name, IF(split='-', 2020 - formed, split - formed) AS lifespan_until_2020
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+WHERE main_style = 'Glam rock'
+ORDER BY ifespan_until_2020 DESC;
