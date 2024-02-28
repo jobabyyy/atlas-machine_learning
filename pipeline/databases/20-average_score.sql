7c1352904cf7efe1ba610038b755fc59683cc6bb
Johanna Avila <joeannaavila123@gmail.com>
2024-02-28 14:21:32 -0600

    changed decimal

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(5,2);
+    DECLARE avg_score DECIMAL(10, 2);
     SELECT AVG(score) INTO avg_score
     FROM user_scores
     WHERE user_id = user_id;
