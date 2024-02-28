9a37ab47018ef29e67866c4f3bb16b7e5196a32d
Johanna Avila <joeannaavila123@gmail.com>
2024-02-27 21:11:46 -0600

    genres added to database

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- list all shows in db by genre and title
+-- list all shows in db by genre and title.
 SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows
 JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
