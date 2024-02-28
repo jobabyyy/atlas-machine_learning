42304b62328717bd1313337f94dbcf21ef28db14
Johanna Avila <joeannaavila123@gmail.com>
2024-02-28 14:18:18 -0600

    computing avg score

---
+++ pipeline/databases/20-average_score.sql
@@ -0,0 +1,21 @@
+-- computes and stores the avg score for a student
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score DECIMAL(5,2);
+    SELECT AVG(score) INTO avg_score
+    FROM user_scores
+    WHERE user_id = user_id;
+    IF EXISTS (SELECT 1 FROM user_average_scores WHERE user_id = user_id)
+        THEN
+        UPDATE user_average_scores
+        SET average_score = avg_score
+        WHERE user_id = user_id;
+    ELSE
+        INSERT INTO user_average_scores (user_id, average_score)
+        VALUES (user_id, avg_score);
+    END IF;
+END$$
+
+DELIMITER ;
