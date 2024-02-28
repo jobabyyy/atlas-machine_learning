616b8853f9c89eae230c82d4f953d2fccba55b30
Johanna Avila <joeannaavila123@gmail.com>
2024-02-28 15:49:38 -0600

    mking updates

--- pipeline/databases/32-update_topics.py
+++ pipeline/databases/32-update_topics.py
@@ -9,7 +9,7 @@ from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
     """
-    Updates the topics of a school doc  based on school name.
+    Update the topics of a school doc  based on school name.
     """
 
-    update_collection({"name": name}, {"$set": {"topics": topics}})
+    update_collection.update_many({"name": name}, {"$set": {"topics": topics}})
