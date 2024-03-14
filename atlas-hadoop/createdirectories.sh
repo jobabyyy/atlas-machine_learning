251038bc3233bca632df9f9c13f458e9fc2c1d23
Johanna Avila <joeannaavila123@gmail.com>
2024-03-14 09:54:37 -0500

    typo - changed dsf to dfs

--- atlas-hadoop/createdirectories.sh
+++ atlas-hadoop/createdirectories.sh
@@ -2,7 +2,7 @@
 # HADOOP - bash scrip that creats the directory 'createdirectories'.
 
 # holbies/ directory
-hdfs dsf -mkdir -p /holbies
+hdfs dfs -mkdir -p /holbies
 
 # now create the /holbies/input directpry
 hdfs dfs -mkdir -p /holbies/input
